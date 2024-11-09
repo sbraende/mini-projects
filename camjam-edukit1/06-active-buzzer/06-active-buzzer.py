a6b8846862604d0ab1148101881b4385b35c189b
Sebastian <sebastian.braende@gmail.com>
2024-11-09 15:46:24 +0100

    Class created for code

--- camjam-edukit1/06-active-buzzer/06-active-buzzer.py
+++ camjam-edukit1/06-active-buzzer/06-active-buzzer.py
@@ -2,74 +2,95 @@ import os
 import time
 from gpiozero import Buzzer, LED
 
-buzzer = Buzzer(22)
-led = LED(18)
-unit = 0.2
-space_between_letter = unit * 3
-space_words = unit * 7
+class morsecode_generator():
+    def __init__(self, buzzer_pin:int, led_pin:int):
+        self.buzzer = Buzzer(buzzer_pin)
+        self.led = LED(led_pin)
+        self.unit = 0.2
+        self.unit_dot = self.unit
+        self.unit_dash = self.unit * 3
+        self.space_between_parts_letters = self.unit
+        self.space_between_letter = self.unit * 3
+        self.space_words = self.unit * 7
 
-def dot():
-    buzzer.on()
-    led.on()
-    time.sleep(unit)
-    buzzer.off()
-    led.off()
+        self.morsecode_table = {
+            "A": ["dot", "dash"],
+            "B": ["dash", "dot", "dot"],
+            "C": ["dash", "dot", "dash", "dot"],
+            "D": ["dash", "dot", "dot"],
+            "E": ["dot"],
+            "F": ["dot", "dot", "dash", "dot"],
+            "G": ["dash", "dash", "dot"],
+            "H": ["dot", "dot", "dot", "dot"],
+            "I": ["dot", "dot"],
+            "J": ["dot", "dash", "dash", "dash"],
+            "K": ["dash", "dot", "dash"],
+            "L": ["dot", "dash", "dot", "dot"],
+            "M": ["dash", "dash"],
+            "N": ["dash", "dot"],
+            "O": ["dash", "dash", "dash"],
+            "P": ["dot", "dash", "dash", "dot"],
+            "Q": ["dash", "dash", "dot", "dash"],
+            "R": ["dot", "dash", "dot"],
+            "S": ["dot", "dot", "dot"], 
+            "T": ["dash"],
+            "U": ["dot", "dot", "dash"],
+            "V": ["dot", "dot", "dot", "dash"],
+            "W": ["dot", "dash", "dash"],
+            "X": ["dash", "dot", "dot", "dash"],
+            "Y": ["dash", "dot", "dash", "dash"],
+            "Z": ["dash", "dash", "dot", "dot"],
+            "0": ["dash", "dash", "dash", "dash", "dash"],
+            "1": ["dot", "dash", "dash", "dash", "dash"],
+            "2": ["dot", "dot", "dash", "dash", "dash"],
+            "3": ["dot", "dot", "dot", "dash", "dash"],
+            "4": ["dot", "dot", "dot", "dot", "dash"],
+            "5": ["dot", "dot", "dot", "dot", "dot"],
+            "6": ["dash", "dot", "dot", "dot", "dot"],
+            "7": ["dash", "dash", "dot", "dot", "dot"],
+            "8": ["dash", "dash", "dash", "dot", "dot"],
+            "9": ["dash", "dash", "dash", "dash", "dot"]
+        }
 
-def dash():
-    buzzer.on()
-    led.on()
-    time.sleep(unit * 3)
-    buzzer.off()
-    led.off()
+    def dot(self):
+        self.buzzer.on()
+        self.led.on()
+        time.sleep(self.unit_dot)
+        self.buzzer.off()
+        self.led.off()
 
-morsecode = {
-    "A": [dot, dash],
-    "B": [dash, dot, dot],
-    "C": [dash, dot, dash, dot],
-    "D": [dash, dot, dot],
-    "E": [dot],
-    "F": [dot, dot, dash, dot],
-    "G": [dash, dash, dot],
-    "H": [dot, dot, dot, dot],
-    "I": [dot, dot],
-    "J": [dot, dash, dash, dash],
-    "K": [dash, dot, dash],
-    "L": [dot, dash, dot, dot],
-    "M": [dash, dash],
-    "N": [dash, dot],
-    "O": [dash, dash, dash],
-    "P": [dot, dash, dash, dot],
-    "Q": [dash, dash, dot, dash],
-    "R": [dot, dash, dot],
-    "S": [dot, dot, dot], 
-    "T": [dash],
-    "U": [dot, dot, dash],
-    "V": [dot, dot, dot, dash],
-    "W": [dot, dash, dash],
-    "X": [dash, dot, dot, dash],
-    "Y": [dash, dot, dash, dash],
-    "Z": [dash, dash, dot, dot],
-    "0": [dash, dash, dash, dash, dash],
-    "1": [dot, dash, dash, dash, dash],
-    "2": [dot, dot, dash, dash, dash],
-    "3": [dot, dot, dot, dash, dash],
-    "4": [dot, dot, dot, dot, dash],
-    "5": [dot, dot, dot, dot, dot],
-    "6": [dash, dot, dot, dot, dot],
-    "7": [dash, dash, dot, dot, dot],
-    "8": [dash, dash, dash, dot, dot],
-    "9": [dash, dash, dash, dash, dot]
-}
+    def dash(self):
+        self.buzzer.on()
+        self.led.on()
+        time.sleep(self.unit_dash)
+        self.buzzer.off()
+        self.led.off()
 
-userinput = "help me".upper()
+    def message(self):
+        return input("Message to convert to morse code:").upper()
 
-for letter in userinput:
-    if letter == " ":
-        print("there is a space here")
-        time.sleep(space_words)
-    elif letter in morsecode:
-        for call_function in morsecode[letter]:
-            call_function()
-            time.sleep(space_between_letter)
-    else:
-        print("Symbol unsupported, skipped")
+    def play(self, message):
+        for symbol in message:
+            if symbol == " ":
+                print("there is a space here")
+                time.sleep(self.space_words)
+            elif symbol in self.morsecode_table:
+                for value in self.morsecode_table[symbol]:
+                    if value == "dot":
+                        self.dot()
+                    elif value == "dash":
+                        self.dash()
+                    else:
+                        print("Something has gone wrong...")
+                    time.sleep(self.space_between_letter)
+            else:
+                print("Symbol unsupported, skipped")
+        print("Message done")
+
+def main():
+    moresecode = morsecode_generator(22, 18)
+    message = moresecode.message()
+    moresecode.play(message)
+main()
+
+
